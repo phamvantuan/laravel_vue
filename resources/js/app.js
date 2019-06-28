452895716218f414eef23df14a498dac2fdea3fd
bravesoft <tuanphambravesoft@gmail.com>
2019-06-28 17:46:12 +0700

    create user

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,10 +5,37 @@
  */
 
 require('./bootstrap');
-
-
 window.Vue = require('vue');
+import moment from 'moment';
 
+import { Form, HasError, AlertError } from 'vform'
+window.Form = Form;
+
+Vue.component(HasError.name, HasError)
+Vue.component(AlertError.name, AlertError)
+
+
+import VueRouter from 'vue-router'
+Vue.use(VueRouter);
+
+let routes = [
+	{path: '/dashboard', component: require('./components/Dashboard.vue').default},
+	{path: '/profile', component: require('./components/Profile.vue').default},
+	{path: '/users', component: require('./components/Users.vue').default}
+]
+
+const router = new VueRouter({
+	mode: 'history',
+	routes
+})
+
+Vue.filter('upText', function(text){
+	return text.charAt(0).toUpperCase() + text.slice(1);
+});
+
+Vue.filter('myDate', function(created){
+	return moment(created).format('MMMM Do YYYY');
+})
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
@@ -30,4 +57,5 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 
 const app = new Vue({
     el: '#app',
+    router
 });
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -14,7 +14,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <link rel="stylesheet" href="/css/app.css">
 </head>
 <body class="hold-transition sidebar-mini">
-<div class="wrapper">
+<div class="wrapper" id="app">
 
   <!-- Navbar -->
   <nav class="main-header navbar navbar-expand bg-white navbar-light border-bottom">
@@ -29,9 +29,9 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
     <!-- SEARCH FORM -->
       <div class="input-group input-group-sm">
-        <input class="form-control form-control-navbar" @keyup="searchit" v-model="search" type="search" placeholder="Search" aria-label="Search">
+        <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
         <div class="input-group-append">
-          <button class="btn btn-navbar" @click="searchit">
+          <button class="btn btn-navbar">
             <i class="fa fa-search"></i>
           </button>
         </div>
@@ -42,101 +42,67 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
-    <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
-      <img src="./img/logo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
-           style="opacity: .8">
-      <span class="brand-text font-weight-light">Lara Start</span>
-    </a>
+   
+   <nav class="mt-2">
+    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+        <li class="nav-item">
+          <router-link to="/dashboard" class="nav-link">
+            <i class="nav-icon fas fa-tachometer-alt"></i>
+            <p>Dashboard</p>
+          </router-link>
+        </li>
 
-    <!-- Sidebar -->
-    <div class="sidebar">
-      <!-- Sidebar user panel (optional) -->
-      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-        <div class="image">
-          <img src="./img/profile.png" class="img-circle elevation-2" alt="User Image">
-        </div>
-        <div class="info">
-          <a href="#" class="d-block">
-              {{Auth::user()->name}}
-              <p>{{Auth::user()->type}}</p>
+        <li class="nav-item has-treeview">
+          <a href="" class="nav-link">
+            <i class="nav-icon fa fa-cog"></i>
+            <p>Management
+              <i class="right fa fa-angle-left"></i>
+            </p>
+            
           </a>
-        </div>
-      </div>
 
-      <!-- Sidebar Menu -->
-      <nav class="mt-2">
-        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-          <!-- Add icons to the links using the .nav-icon class
-               with font-awesome or any other icon font library -->
-
+          <ul class="nav nav-treeview">
             <li class="nav-item">
-            <router-link to="/dashboard" class="nav-link">
-                <i class="nav-icon fas fa-tachometer-alt blue"></i>
-                <p>
-                Dashboard
-
-                </p>
-            </router-link>
+              <router-link to="/users" class="nav-link">
+                <i class="fa fa-users nav-icon"></i>
+                <p>Users</p>
+              </router-link>
             </li>
 
-            @can('isAdmin')
-          <li class="nav-item has-treeview">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fa fa-cog green"></i>
-              <p>
-                Management
-                <i class="right fa fa-angle-left"></i>
-              </p>
-            </a>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <router-link to="/users" class="nav-link">
-                  <i class="fas fa-users nav-icon"></i>
-                  <p>Users</p>
-                </router-link>
-              </li>
+    
+          </ul>
 
-            </ul>
-          </li>
-
-          <li class="nav-item">
-                <router-link to="/developer" class="nav-link">
-                    <i class="nav-icon fas fa-cogs"></i>
-                    <p>
-                        Developer
-                    </p>
-                </router-link>
-         </li>
-         @endcan
-          <li class="nav-item">
-                <router-link to="/profile" class="nav-link">
-                    <i class="nav-icon fas fa-user orange"></i>
-                    <p>
-                        Profile
-                    </p>
-                </router-link>
-         </li>
-
-          <li class="nav-item">
-                <a class="nav-link" href="{{ route('logout') }}"
-                onclick="event.preventDefault();
-                              document.getElementById('logout-form').submit();">
-                    <i class="nav-icon fa fa-power-off red"></i>
-                    <p>
-                        {{ __('Logout') }}
-                    </p>
-                 </a>
-
-             <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                 @csrf
-             </form>
         </li>
-        </ul>
-      </nav>
-      <!-- /.sidebar-menu -->
-    </div>
-    <!-- /.sidebar -->
+
+
+
+
+
+        <li class="nav-item">
+          <router-link to="/profile" class="nav-link">
+            <i class="nav-icon fas fa-user"></i>
+            <p>Profile</p>
+          </router-link>
+        </li>
+
+        <li class="nav-item">
+                             
+        <a class="nav-link" href="{{ route('logout') }}"
+           onclick="event.preventDefault();
+                         document.getElementById('logout-form').submit();">
+                         <i class="nav-icon fas fa-power-off"></i>
+            <p>Logout</p>
+        </a>
+
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+            @csrf
+        </form>
+                              
+                          
+        </li>
+    </ul>
+     
+   </nav>
   </aside>
 
   <!-- Content Wrapper. Contains page content -->
@@ -145,7 +111,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <!-- Main content -->
     <div class="content">
       <div class="container-fluid">
-
+        <router-view></router-view>
       </div><!-- /.container-fluid -->
     </div>
     <!-- /.content -->
